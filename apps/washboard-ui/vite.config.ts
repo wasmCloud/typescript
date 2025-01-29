da42b4d57dcb4d5ad34124738e98a0d85bdbf4d9
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-01-29 01:01:50 -0500

    chore: update to tailwind v4
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- apps/washboard-ui/vite.config.ts
+++ apps/washboard-ui/vite.config.ts
@@ -3,10 +3,11 @@ import sourceMaps from 'rollup-plugin-sourcemaps';
 import {defineConfig} from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgrPlugin()],
+  plugins: [tailwindcss(), react(), tsconfigPaths(), svgrPlugin()],
   build: {
     sourcemap: true,
     rollupOptions: {
