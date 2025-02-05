3eceb2e8ca1fd995c585ea6e4a21481188a5603f
Victor Adossi <vadossi@cosmonic.com>
2025-02-06 04:00:10 +0900

    fix(examples): hello world generated types
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- examples/components/http-hello-world/generated/types/interfaces/wasi-clocks-monotonic-clock.d.ts
+++ examples/components/http-hello-world/generated/types/interfaces/wasi-clocks-monotonic-clock.d.ts
@@ -1,4 +1,4 @@
-declare module 'wasi:clocks/monotonic-clock@0.2.0' {
+declare module 'wasi:clocks/monotonic-clock@0.2.2' {
   export function now(): Instant;
   export function resolution(): Duration;
   export function subscribeInstant(when: Instant): Pollable;
