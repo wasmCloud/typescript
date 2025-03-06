742073eb6f74073f0907596deda57a0d82f3b9cc
Victor Adossi <vadossi@cosmonic.com>
2025-03-06 20:25:08 +0900

    fix: streaming example
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>
    Signed-off-by: Thomas Timmer <thomas.timmer@bettyblocks.com>

--- examples/components/http-streaming/src/http-streaming.ts
+++ examples/components/http-streaming/src/http-streaming.ts
@@ -1,60 +1,97 @@
+/**
+ * This component showcases how to do performant HTTP streaming with the HTTP and I/O
+ * interfaces provided by the WebAssembly System Interface (WASI), in WASI P2.
+ *
+ * For more information on the interfaces and how to use them:
+ * - https://github.com/WebAssembly/WASI/tree/main/wasip2
+ * - https://github.com/WebAssembly/wasi-http
+ * - https://github.com/WebAssembly/wasi-io
+ */
+
 import {
   IncomingRequest,
   ResponseOutparam,
   OutgoingBody,
   OutgoingResponse,
   Fields,
-} from 'wasi:http/types@0.2.3';
+} from "wasi:http/types@0.2.3";
 
-import {
- getRandomBytes
-} from 'wasi:random/random@0.2.3';
+import { getRandomBytes } from "wasi:random/random@0.2.3";
 
+/**
+ * Generate random data to send over the write as our streaming workload
+ */
 function generateData(n: bigint): string {
-  return btoa(getRandomBytes(n).toString())
+  return btoa(getRandomBytes(n).toString());
 }
 
-// Implementation of wasi-http incoming-handler
-//
-// NOTE: To understand the types involved, take a look at wit/deps/http/types.wit
+/**
+ * Implementation of wasi-http incoming-handler
+ *
+ * To understand the types involved, peruse the following files:
+ *   - wit/world.wit
+ *   - wit/deps/http/types.wit
+ *
+ */
 function handle(req: IncomingRequest, resp: ResponseOutparam) {
   // Start building an outgoing response
   const outgoingResponse = new OutgoingResponse(new Fields());
 
   // Access the outgoing response body
   let outgoingBody = outgoingResponse.body();
-  {
-    let outputStream = outgoingBody.write();
+  let outputStream = outgoingBody.write();
 
-    // generate text
-    let text = generateData(8n);
-    let data = new Uint8Array(new TextEncoder().encode(text));
+  // Generate data to send out
+  //
+  // The amount to send is arbitrarily chosen, but over 4096 to make
+  // things more interesting than using `wasi:io/streams.blocking-write-and-flush`
+  let text = generateData(8192n);
+  let data = new Uint8Array(new TextEncoder().encode(text));
 
-    let pollable = outputStream.subscribe();
-    let written = 0n;
-    while (written < data.length) {
-        // Wait for the stream to become writable
-        pollable.block();
-        let n = outputStream.checkWrite();
-        let sub = new Uint8Array(data, Number(written), Number(written + n));
-        outputStream.write(sub);
-    }
-    outputStream.flush();
+  // Retrieve a Preview 2 I/O pollable to coordinate writing to the output stream
+  let pollable = outputStream.subscribe();
+  let written = 0n;
+  let remaining = BigInt(data.length);
+  while (remaining > 0) {
+    // Wait for the stream to become writable
     pollable.block();
 
-    // TODO: blockingWriteAndFlush just works:
-    // outputStream.blockingWriteAndFlush(data);
+    // Get the amount of bytes that we're allowed to write
+    let writableByteCount = outputStream.checkWrite();
+    if (remaining <= writableByteCount) {
+      writableByteCount = BigInt(remaining);
+    }
 
-    // @ts-ignore: This is required in order to dispose the stream before we return
-    outputStream[Symbol.dispose]();
+    // If we are not allowed to write any more, but there are still bytes
+    // remaining then flush and try again
+    if (writableByteCount === 0n && remaining !== 0n) {
+      outputStream.flush();
+      continue;
+    }
+
+    outputStream.write(
+      new Uint8Array(
+        data.buffer,
+        Number(written),
+        Number((written += writableByteCount)) // NOTE: written is updated
+      )
+    );
+    remaining -= written;
+
+    // While we can track *when* to flush separately and implement our own logic,
+    // the simplest way is to flush the written chunk immediately
+    outputStream.flush();
   }
 
-  // Set the status code for the response
+  // We must clean up the resources we've created to facilitate polling
+  // @ts-ignore: While TS does not *know* that the dispose symbols are registered, they are.
+  pollable[Symbol.dispose]();
+  // @ts-ignore: While TS does not *know* that the dispose symbols are registered, they are.
+  outputStream[Symbol.dispose]();
+
   outgoingResponse.setStatusCode(200);
-  // Finish the response body
   OutgoingBody.finish(outgoingBody, undefined);
-  // Set the created response
-  ResponseOutparam.set(resp, { tag: 'ok', val: outgoingResponse });
+  ResponseOutparam.set(resp, { tag: "ok", val: outgoingResponse });
 }
 
 export const incomingHandler = {
