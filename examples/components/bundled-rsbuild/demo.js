65a2e514d95d65c54ca42b999bd481c65d859046
Thomas Timmer <thomas.timmer@bettyblocks.com>
2025-04-08 17:55:56 +0200

    feat: update and use typescript types from wit folder
    Signed-off-by: Thomas Timmer <thomas.timmer@bettyblocks.com>

--- examples/components/bundled-rsbuild/demo.js
+++ examples/components/bundled-rsbuild/demo.js
@@ -1,5 +1,6 @@
-import { csvUtils } from "./dist/transpiled/http_bundling_s.js"
+import { csvUtils } from "./dist/transpiled/bundled_rsbuild_s.js"
 
-const DATA = "title,score\nHello,5\nWorld,10"
+const CSV_DATA = "title,score\nHello,3\nWorld,7"
+const JSON_DATA = JSON.stringify([{ title: "Hello", score: 5 }]);
 
-console.log(csvUtils.calculateScore(DATA))
+console.log(csvUtils.calculateScore({ tag: "csv", val: CSV_DATA }) + csvUtils.calculateScore({ tag: "json", val: JSON_DATA }))
--- examples/components/bundled-rsbuild/src/bundled-rsbuild.ts
+++ examples/components/bundled-rsbuild/src/bundled-rsbuild.ts
@@ -1,19 +1,25 @@
-
 import csv from "csvtojson";
 import jsonpath from "jsonpath";
 import * as R from "remeda";
+import { InputData } from "wasmcloud:hello/csvUtils";
 
-async function calculateScore(data: string): Promise<number> {
+async function calculateScore(data: InputData): Promise<number> {
+  let json = {};
+  if (data.tag === "csv") {
+    json = await csv().fromString(data.val);
+  } else if (data.tag === "json") {
+    json = JSON.parse(data.val);
+  }
+
   // Bit of a contrived example to use typescript dependencies
-  const json = await csv().fromString(data);
-  const scoresData: string[] = jsonpath.query(json, '$[*].score')
+  const scoresData: string[] = jsonpath.query(json, "$[*].score");
   const total = R.piped(
     R.map((x: unknown) => parseInt(x as string)),
-    R.reduce((acc, x: number) => acc + x, 0)
-  )(scoresData)
-  return total
+    R.reduce((acc, x: number) => acc + x, 0),
+  )(scoresData);
+  return total;
 }
 
 export const csvUtils = {
-  calculateScore
-}
+  calculateScore,
+};
