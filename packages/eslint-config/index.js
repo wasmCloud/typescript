24560ee5921ef536311ad19b437beaafba61d41c
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-05-06 15:52:54 -0400

    feat!(eslint-config): update repo config to eslint 9 w/ flat configs
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,158 +1,154 @@
-const path = require('node:path');
+import {defineConfig, globalIgnores} from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import {configs as tsEslintConfigs} from 'typescript-eslint';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintComments from '@eslint-community/eslint-plugin-eslint-comments/configs';
+import unicorn from 'eslint-plugin-unicorn';
+import {flatConfigs as eslintImportX} from 'eslint-plugin-import-x';
+import prettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 
-module.exports = {
-  root: true,
-  env: {browser: true, es2020: true},
-  extends: [
-    'eslint:recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'plugin:@typescript-eslint/recommended',
-    // TODO: Disabled until update to support TailwindCSS v4
-    // https://github.com/francoismassart/eslint-plugin-tailwindcss/issues/325
-    // 'plugin:tailwindcss/recommended',
-    'plugin:eslint-comments/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:unicorn/recommended',
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json', './tsconfig.eslint.json'],
-  },
-  plugins: ['react-refresh'],
-  rules: {
-    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-    '@typescript-eslint/explicit-member-accessibility': ['warn', {accessibility: 'no-public'}],
-    '@typescript-eslint/member-ordering': [
-      'warn',
-      {
-        default: 'never',
-        classes: ['field', ['get', 'set'], 'constructor', 'method'],
+export default defineConfig([
+  unicorn.configs.recommended,
+  js.configs.recommended,
+  eslintImportX.recommended,
+  eslintImportX.typescript,
+  tsEslintConfigs.recommended,
+  eslintComments.recommended,
+  react.configs.flat.recommended,
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        project: ['./tsconfig.json', './tsconfig.eslint.json'],
       },
-    ],
-    '@typescript-eslint/no-loss-of-precision': 'warn',
-    '@typescript-eslint/no-unused-vars': ['warn', {ignoreRestSiblings: true}],
-    'eslint-comments/disable-enable-pair': ['error', {allowWholeFile: true}],
-    'eslint-comments/no-unused-disable': 'error',
-    'eslint-comments/require-description': 'warn',
-    'import/no-cycle': ['error', {ignoreExternal: false}],
-    'import/no-named-as-default-member': 'off',
-    'import/order': [
-      'warn',
-      {
-        alphabetize: {
-          order: 'asc',
+    },
+
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/explicit-member-accessibility': ['warn', {accessibility: 'no-public'}],
+      '@typescript-eslint/member-ordering': [
+        'warn',
+        {
+          default: 'never',
+          classes: ['field', ['get', 'set'], 'constructor', 'method'],
         },
-        groups: [
-          'unknown',
-          'type',
-          'builtin',
-          'external',
-          'internal',
-          'object',
-          'parent',
-          'sibling',
-          'index',
-        ],
-        pathGroups: [
-          {
-            group: 'unknown',
-            pattern: '**/*.+(css|sass|scss|less|styl)',
-            patternOptions: {dot: true, nocomment: true},
-            position: 'before',
+      ],
+      '@typescript-eslint/no-loss-of-precision': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', {ignoreRestSiblings: true}],
+      '@eslint-community/eslint-comments/disable-enable-pair': ['error', {allowWholeFile: true}],
+      '@eslint-community/eslint-comments/no-unused-disable': 'error',
+      '@eslint-community/eslint-comments/require-description': 'warn',
+      'import-x/no-cycle': ['error', {ignoreExternal: false}],
+      'import-x/no-named-as-default-member': 'off',
+      'import-x/order': [
+        'warn',
+        {
+          alphabetize: {
+            order: 'asc',
           },
-          {
-            group: 'unknown',
-            pattern: '{.,..}/**/*.+(css|sass|scss|less|styl)',
-            patternOptions: {dot: true, nocomment: true},
-            position: 'before',
-          },
-        ],
-        warnOnUnassignedImports: true,
-      },
-    ],
-    'import/no-default-export': 'error',
-    'no-case-declarations': 'warn',
-    'no-console': ['warn', {allow: ['info', 'warn', 'error']}],
-    'no-param-reassign': 'warn',
-    'no-restricted-imports': [
-      'error',
-      {
-        patterns: [
-          {
-            group: ['../../', './../../'],
-            message: 'Relative imports are not allowed. Please use the @/ alias.',
-          },
-        ],
-      },
-    ],
-    'no-restricted-syntax': [
-      'error',
-      {
-        selector: ':matches(PropertyDefinition, MethodDefinition)[accessibility="private"]',
-        message: 'Use #private instead',
-      },
-    ],
-    'no-undef': 'warn',
-    'no-unneeded-ternary': 'warn',
-    'no-unreachable': 'warn',
-    'object-curly-spacing': ['warn', 'never'],
-    'react-refresh/only-export-components': 'warn',
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'spaced-comment': ['warn', 'always', {markers: ['/']}],
-    'unicorn/no-array-reduce': 'off',
-    'unicorn/no-null': 'off',
-    'unicorn/prevent-abbreviations': [
-      'error',
-      {
-        replacements: {
-          args: false,
-          prop: false,
-          props: false,
-          ref: false,
-          refs: false,
+          groups: [
+            'unknown',
+            'type',
+            ['object', 'builtin', 'external'],
+            'internal',
+            ['parent', 'sibling', 'index'],
+          ],
+          'newlines-between': 'always',
+          pathGroupsExcludedImportTypes: ['builtin', 'object'],
+          pathGroups: [
+            {
+              pattern: '**/*.+(css|sass|scss|less|styl)',
+              group: 'unknown',
+              patternOptions: {dot: true, nocomment: true},
+              position: 'before',
+            },
+            {
+              pattern: '@wasmcloud/**',
+              group: 'internal',
+              position: 'before',
+            },
+          ],
+          warnOnUnassignedImports: true,
         },
+      ],
+      'import-x/no-default-export': 'error',
+      'no-case-declarations': 'warn',
+      'no-console': ['warn', {allow: ['info', 'warn', 'error']}],
+      'no-param-reassign': 'warn',
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ['../../', './../../'],
+              message: 'Relative imports are not allowed. Please use the @/ alias.',
+            },
+          ],
+        },
+      ],
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: ':matches(PropertyDefinition, MethodDefinition)[accessibility="private"]',
+          message: 'Use #private instead',
+        },
+      ],
+      'no-undef': 'warn',
+      'no-unneeded-ternary': 'warn',
+      'no-unreachable': 'warn',
+      'object-curly-spacing': ['warn', 'never'],
+      'react-refresh/only-export-components': 'warn',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'spaced-comment': ['warn', 'always', {markers: ['/']}],
+      'unicorn/no-array-reduce': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/prevent-abbreviations': [
+        'error',
+        {
+          replacements: {
+            args: false,
+            prop: false,
+            props: false,
+            ref: false,
+            refs: false,
+          },
+        },
+      ],
+    },
+
+    settings: {
+      'import-x/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
       },
-    ],
+      'import-x/resolver': {
+        typescript: {alwaysTryTypes: true},
+      },
+      react: {
+        version: 'detect',
+      },
+    },
   },
-  overrides: [
-    {
-      files: ['*@(rc|.config).@(cjs|js|ts)', 'tests/**/*.ts'],
-      env: {
-        node: true,
-      },
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off',
-        'import/no-default-export': 'off',
-      },
+  {
+    files: ['**/*@(rc|.config).@(cjs|js|ts)', 'tests/**/*.ts'],
+    languageOptions: {
+      globals: globals.node,
     },
-  ],
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: [
-          path.resolve(__dirname, './tsconfig.json'),
-          path.resolve(__dirname, './tsconfig.eslint.json'),
-        ],
-      },
-    },
-    react: {
-      version: 'detect',
-    },
-    tailwindcss: {
-      cssFiles: [],
-      callees: ['classnames', 'clsx', 'ctl', 'cn', 'cva'],
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+      'import-x/no-default-export': 'off',
     },
   },
-  ignorePatterns: ['node_modules', 'dist', 'build', 'coverage'],
-};
+  prettier,
+  globalIgnores(['**/node_modules', '**/dist', '**/build', '**/coverage']),
+]);
