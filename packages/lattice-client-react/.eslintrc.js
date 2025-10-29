19f56f887fa801e2f4fc8bb61a5f06e4fcd98e7d
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-10-29 17:05:38 -0400

    chore(eslint-config): enable turbo config
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- packages/lattice-client-react/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-export default {
-  extends: ['@wasmcloud/eslint-config'],
-  parserOptions: {
-    tsconfigRootDir: import.meta.dirname,
-    project: ['./tsconfig.eslint.json', './tsconfig.json'],
-  },
-};
