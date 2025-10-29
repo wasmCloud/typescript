d500ffe389f70601732764ba9f326c86285e23f1
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-10-29 16:17:55 -0400

    chore(washboard-ui): update tailwind across packages and apps
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- apps/washboard-ui/src/components/select/index.tsx
+++ apps/washboard-ui/src/components/select/index.tsx
@@ -50,7 +50,7 @@ const SelectContent = React.forwardRef<
         className={cn(
           'p-1',
           position === 'popper' &&
-            'h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]',
+            'h-(--radix-select-trigger-height) w-full min-w-(--radix-select-trigger-width)',
         )}
       >
         {children}
